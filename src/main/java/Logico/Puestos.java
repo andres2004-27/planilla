6acb1b646b37bdd95a2d3311124fa535fe4258bf
Andres424 <andresjesusgut424@gmail.com>
2025-08-04 14:15:04 -0600

    Creacion de la clase Puesto

---
+++ src/main/java/Logico/Puestos.java
@@ -0,0 +1,48 @@
+
+package Logico;
+
+/**
+ *
+ * @author Andrés
+ */
+public class Puestos {
+    private int idPuesto;
+    private String nomPuesto;
+    private double Salario;
+
+    public Puestos(int idPuesto, String nomPuesto, double Salario) {
+        this.idPuesto = idPuesto;
+        this.nomPuesto = nomPuesto;
+        this.Salario = Salario;
+    }
+    public Puestos() {
+        this.idPuesto = 0;
+        this.nomPuesto = "";
+        this.Salario = 0;
+        
+    
+}
+
+    public int getIdPuesto() {
+        return idPuesto;
+    }
+
+    public void setIdPuesto(int idPuesto) {
+        this.idPuesto = idPuesto;
+    }
+
+    public String getNomPuesto() {
+        return nomPuesto;
+    }
+
+    public void setNomPuesto(String nomPuesto) {
+        this.nomPuesto = nomPuesto;
+    }
+
+    public double getSalario() {
+        return Salario;
+    }
+
+    public void setSalario(double Salario) {
+        this.Salario = Salario;
+    }
